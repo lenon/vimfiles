7bd2bb6a6062e3866e29eca22da3cacd986b2705
Lenon Marcel <lenon.marcel@gmail.com>
2016-02-28 21:16:40 -0300

    Symlink neovim config file

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 ln -s ~/vimfiles/vimrc ~/.vimrc
 ln -s ~/vimfiles/gvimrc ~/.gvimrc
+mkdir -p ~/.config/nvim
+ln -s ~/.config/nvim/init.vim ~/vimfiles/vimrc
 vim +PluginInstall +qall
